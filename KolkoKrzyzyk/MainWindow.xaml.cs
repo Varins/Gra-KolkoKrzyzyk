1bde4b9564db6e8c30dc20e036ee103b9c2d9a8b
Varins <marinsans001@gmail.com>
2021-05-04 09:27:36 +0200

    grid button click handler added

--- KolkoKrzyzyk/MainWindow.xaml.cs
+++ KolkoKrzyzyk/MainWindow.xaml.cs
@@ -25,5 +25,79 @@ namespace KolkoKrzyzyk
         {
             InitializeComponent();      
         }
+
+        private void GridButton_Click(object sender, RoutedEventArgs e)
+        {
+            GraKolkoKrzyzykClient client = new GraKolkoKrzyzykClient();
+            Button btn = (Button)sender;
+            int row = 3, col = 3;
+            switch (btn.Name)
+            {
+                case ("Btn0"):
+                    {
+                        col = 0;
+                        row = 0;
+                        break;
+                    }
+                case ("Btn1"):
+                    {
+                        col = 1;
+                        row = 0;
+                        break;
+                    }
+                case ("Btn2"):
+                    {
+                        col = 2;
+                        row = 0;
+                        break;
+                    }
+                case ("Btn3"):
+                    {
+                        col = 0;
+                        row = 1;
+                        break;
+                    }
+                case ("Btn4"):
+                    {
+                        col = 1;
+                        row = 1;
+                        break;
+                    }
+                case ("Btn5"):
+                    {
+                        col = 2;
+                        row = 1;
+                        break;
+                    }
+                case ("Btn6"):
+                    {
+                        col = 0;
+                        row = 2;
+                        break;
+                    }
+                case ("Btn7"):
+                    {
+                        col = 1;
+                        row = 2;
+                        break;
+                    }
+                case ("Btn8"):
+                    {
+                        col = 2;
+                        row = 2;
+                        break;
+                    }
+            }
+            if(col < 3 && row < 3)
+            {
+                int outRow=3, outCol=3;
+                if(client.WykonajRuch(row, col, out outRow, out outRow))
+                {
+                    client.SprawdzWygrana(row, col);
+                    if(outRow + outCol < 5)
+                        client.SprawdzWygrana(outRow, outCol);
+                }
+            }
+        }
     }
 }
